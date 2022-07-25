4ec8350cbf81526089be4157d7b0eef9890ad7ce
rbinnur <rashed.bin.nur@gmail.com>
2022-07-25 18:22:20 -0400

    i am

---
+++ src/main/java/Testing1stclass/class5.java
@@ -0,0 +1,13 @@
+package Testing1stclass;
+
+public class class5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0; i <=10; i++) {
+			System.out.println(i+": i am rafi :" +i);
+
+	}
+
+}
